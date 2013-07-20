dbb308b85d8cbbdf9720114f752dcb74be58dda2
Kenta Murata <mrkn@mrkn.jp>
2013-07-20 16:26:52 +0900

    rails new railsterakoya

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsterakoya::Application.config.secret_key_base = 'e95856d516907555602e806059eaadec93d6d32e73c1261ba3382ec38fbebf66414a85b8c7d080d83e6a6f3476601ca6e7836cd5717eb66fbbb2dfebbd467c9b'
