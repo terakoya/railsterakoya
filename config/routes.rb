5a289257ae5578b1039e8228a0e4bfc08ff6fc04
Kenta Murata <mrkn@mrkn.jp>
2013-07-20 16:29:20 +0900

    rails generate scaffold idea name:string description:text picture:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Railsterakoya::Application.routes.draw do
+  resources :ideas
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
